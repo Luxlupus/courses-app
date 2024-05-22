d078737007b9baa87f26d453a00dcf2a558684fb
Ivan Spiljak <ispiljak@gmail.com>
2024-05-22 17:57:40 +0200

    Created project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,34 @@
+import js from "@eslint/js";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tsParser from "@typescript-eslint/parser";
+import ts from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
 
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+      react,
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detects the react version
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...ts.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      // Additional custom rules can be added here
+    },
+  },
 ];
